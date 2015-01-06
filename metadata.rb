6ab65ff6bbd7287d177b8d8d34f70dcae58d0c46
Jeff Hutchison <jeff@jeffhutchison.com>
2015-01-06 09:10:34 -0500

    Add issues url.

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer       'Jeff Hutchison'
 maintainer_email 'jeff@jeffhutchison.com'
 license          'Apache v2.0'
 source_url       'https://github.com/jhh/firewalld-cookbook'
+issues_url       'https://github.com/jhh/firewalld-cookbook/issues'
 description      'Installs/Configures firewalld'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
