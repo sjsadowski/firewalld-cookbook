55aa1aeaf2f6e1c28e364fb2626c7833e5a11d45
Jeff Hutchison <jeff@jeffhutchison.com>
2015-01-08 14:42:03 -0500

    Clean up syntax.

--- providers/port.rb
+++ providers/port.rb
@@ -7,8 +7,10 @@
 action :add do
   e = execute 'add open port to zone' do
     not_if "firewall-cmd --permanent #{zone} --query-port=#{new_resource.port}"
-    command "firewall-cmd #{zone} --add-port=#{new_resource.port}; " +
-    "firewall-cmd --permanent #{zone} --add-port=#{new_resource.port}"
+    command(<<-EOC)
+      firewall-cmd #{zone} --add-port=#{new_resource.port}
+      firewall-cmd --permanent #{zone} --add-port=#{new_resource.port}
+    EOC
   end
   new_resource.updated_by_last_action(e.updated_by_last_action?)
 end
@@ -16,8 +18,10 @@ end
 action :remove do
   e = execute 'remove open port from zone' do
     only_if "firewall-cmd --permanent #{zone} --query-port=#{new_resource.port}"
-    command "firewall-cmd #{zone} --remove-port=#{new_resource.port}; " +
-    "firewall-cmd --permanent #{zone} --remove-port=#{new_resource.port}"
+    command(<<-EOC)
+      firewall-cmd #{zone} --remove-port=#{new_resource.port}
+      firewall-cmd --permanent #{zone} --remove-port=#{new_resource.port}
+    EOC
   end
   new_resource.updated_by_last_action(e.updated_by_last_action?)
 end
