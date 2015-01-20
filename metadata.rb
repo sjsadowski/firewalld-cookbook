68e3c9181fba0a7ed92959079c730b4f49bec01a
Manny Toledo <mtoledo@adobe.com>
2015-01-20 16:50:47 -0500

    New: Version bump in metadata

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jeff@jeffhutchison.com'
 license          'Apache v2.0'
 description      'Installs/Configures firewalld'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
+version          '0.3.0'
 
 supports         'centos', ">= 7.0"
 supports         'rhel', ">= 7.0"
