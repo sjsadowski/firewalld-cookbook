3ea84b03a8e72e6654634d24e5ccbbe0360acd79
Jeff Hutchison <jeff@jeffhutchison.com>
2015-01-25 13:23:39 -0500

    Version number must be decimal.

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Installs/Configures firewalld'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.1'
 
-supports         'fedora', ">= 15"
+supports         'fedora', ">= 15.0"
 supports         'centos', ">= 7.0"
 supports         'rhel', ">= 7.0"
