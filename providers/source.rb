f0ff6d4ef8b01c3b26c8fce4f86971017bb9ae6a
Johnathan Kupferer <jtk@uic.edu>
2015-04-06 13:44:12 -0500

    Add resource types source and interface.
    These resource types correspond to the firewall-cmd source and
    interface commands.

---
+++ providers/source.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook Name:: firewalld
+# Provider:: source
+#
+# Copyright:: 2015, Johnathan Kuperer
+
+action :add do
+  e = execute "add source #{new_resource.source} to zone" do
+    not_if "firewall-cmd --permanent #{zone} --query-source=#{new_resource.source}"
+    command(<<-EOC)
+      firewall-cmd #{zone} --add-source=#{new_resource.source}
+      firewall-cmd --permanent #{zone} --add-source=#{new_resource.source}
+    EOC
+  end
+  new_resource.updated_by_last_action(e.updated_by_last_action?)
+end
+
+action :change do
+  e = execute "change source #{new_resource.source} to zone" do
+    not_if "firewall-cmd --permanent #{zone} --query-source=#{new_resource.source}"
+    command(<<-EOC)
+      firewall-cmd #{zone} --change-source=#{new_resource.source}
+      firewall-cmd --permanent #{zone} --change-source=#{new_resource.source}
+    EOC
+  end
+  new_resource.updated_by_last_action(e.updated_by_last_action?)
+end
+
+action :remove do
+  e = execute "remove source #{new_resource.source} from zone" do
+    only_if "firewall-cmd --permanent #{zone} --query-source=#{new_resource.source}"
+    command(<<-EOC)
+      firewall-cmd #{zone} --remove-source=#{new_resource.source}
+      firewall-cmd --permanent #{zone} --remove-source=#{new_resource.source}
+    EOC
+  end
+  new_resource.updated_by_last_action(e.updated_by_last_action?)
+end
+
+def zone
+  new_resource.zone ? "--zone=#{new_resource.zone}" : ''
+end
---
+++ resources/source.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: firewalld
+# Provider:: source
+#
+# Copyright:: 2015, Johnathan Kupferer
+
+# List of all actions supported by provider
+actions :add, :change, :remove
+
+# Make add the default action
+default_action :add
+
+# Required attributes
+attribute :source, :kind_of => String, :name_attribute => true
+
+# Optional attributes
+attribute :zone, :kind_of => String
