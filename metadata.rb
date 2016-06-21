9981e5210f183adfee0c8e2bf466829015167728
Ricky Grassmuck <rigrassm@gmail.com>
2016-06-21 04:36:36 +0200

    fix typo in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'firewalld'
 maintainer       'Ricky Grassmuck'
 maintainer_email 'rigrassm@gmail.com'
-source_url       'https://github.com/rigrassm/firewalld-cookbook' if respond_to?(:issues_url
+source_url       'https://github.com/rigrassm/firewalld-cookbook' if respond_to?(:source_url)
 issues_url       'https://github.com/chef-cookbooks/something/issues' if respond_to?(:issues_url)
 license          'Apache v2.0'
 description      'Installs/Configures firewalld'
@@ -13,4 +13,4 @@ supports         'fedora', ">= 15.0"
 supports         'centos', ">= 7.0"
 supports         'rhel',   ">= 7.0"
 supports         'ubuntu', ">= 14.04"
-supports         'debian', ">= 8.0" # untested
+supports         'debian', ">= 8.0" 
