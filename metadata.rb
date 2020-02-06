50d29ceb7ca72349accc610a7d851e04080cc216
Stephen Sadowski <stephen.sadowski@sjsadowski.com>
2020-02-06 07:53:41 -0600

    Bumping to 1.2.0 after rigrassm completed merges and transferred ownership

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ issues_url       'https://github.com/chef-cookbooks/something/issues' if respond
 license          'Apache v2.0'
 description      'Installs/Configures firewalld'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.5'
+version          '1.2.0'
 
 supports         'fedora', ">= 15.0"
 supports         'centos', ">= 7.0"
