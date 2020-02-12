aaa40c53ecee65fef8afd832959639b3683b1f57
Stephen Sadowski <stephen.sadowski@sjsadowski.com>
2020-02-12 07:48:52 -0600

    updated metadata.rb with proper contact info

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'firewalld'
-maintainer       'Ricky Grassmuck'
-maintainer_email 'rigrassm@gmail.com'
-source_url       'https://github.com/rigrassm/firewalld-cookbook' if respond_to?(:source_url)
-issues_url       'https://github.com/chef-cookbooks/something/issues' if respond_to?(:issues_url)
+maintainer       'Stephen Sadowski'
+maintainer_email 'stephen.sadowski@sjsadowski.com'
+source_url       'https://github.com/sjsadowski/firewalld-cookbook' if respond_to?(:source_url)
+issues_url       'https://github.com/sjsadowski/firewalld-cookbook/issues' if respond_to?(:issues_url)
 license          'Apache v2.0'
 description      'Installs/Configures firewalld'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
